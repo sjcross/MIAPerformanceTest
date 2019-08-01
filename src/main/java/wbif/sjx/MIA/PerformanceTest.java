e3892176d21c2f84cbda3a3d90df666e7147fba5
Stephen Cross <stephen.cross@bristol.ac.uk>
2019-08-01 17:25:48 +0100

    Added test object optimiser
    This is based on an example from the internet, but shows how measurements of object coordinate distribution could be used to predict the best Volume type to use.

--- src/main/java/wbif/sjx/MIA/PerformanceTest.java
+++ src/main/java/wbif/sjx/MIA/PerformanceTest.java
@@ -10,6 +10,7 @@ import wbif.sjx.MIA.Object.Obj;
 import wbif.sjx.MIA.Object.ObjCollection;
 import wbif.sjx.MIA.Object.Workspace;
 import wbif.sjx.MIA.Process.Logging.LogRenderer;
+import wbif.sjx.common.MathFunc.CumStat;
 import wbif.sjx.common.Object.Point;
 import wbif.sjx.common.Object.Volume.VolumeType;
 
@@ -23,30 +24,59 @@ public class PerformanceTest {
     private static final String OUTPUT_OBJECTS = "Output objects";
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String path = URLDecoder.decode(PerformanceTest.class.getResource("/performance/Blobs2.tif").getPath(),"UTF-8");
-        runImage(path);
+//        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/BigFilled.tif").getPath(),"UTF-8"));
+//        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/Blobs1.tif").getPath(),"UTF-8"));
+        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/Blobs2.tif").getPath(),"UTF-8"));
+        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/Blobs3.tif").getPath(),"UTF-8"));
+        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/Spot1.tif").getPath(),"UTF-8"));
+        getDistributionResults(URLDecoder.decode(PerformanceTest.class.getResource("/performance/Squares.tif").getPath(),"UTF-8"));
 
     }
 
-    public static void runImage(String path) {
+    public static void getDistributionResults(String path) {
+        // Getting each volume
+        Object[] pointVol = runTest(path, Image.VolumeTypes.POINTLIST,false);
+        Object[] quadtreeVol = runTest(path,Image.VolumeTypes.QUADTREE,false);
+        Object[] octreeVol = runTest(path,Image.VolumeTypes.OCTREE,false);
+
+        // Report distribution for smallest object
+        if ((long) pointVol[1] < (long) quadtreeVol[1] && (long) pointVol[1] < (long) octreeVol[1]) {
+            reportDistribution((Obj) pointVol[0]);
+            System.out.println(",point");
+        } else  if ((long) quadtreeVol[1] < (long) pointVol[1] && (long) quadtreeVol[1] < (long) octreeVol[1]) {
+            reportDistribution((Obj) pointVol[0]);
+            System.out.println(",quadtree");
+        } if ((long) octreeVol[1] < (long) pointVol[1] && (long) octreeVol[1] < (long) quadtreeVol[1]) {
+            reportDistribution((Obj) pointVol[0]);
+            System.out.println(",octree");
+        }
+    }
+
+    public static void comparePerformances(String path) {
         // Testing PointVolume
-        System.out.println("PointVolume");
-        runTest(path, Image.VolumeTypes.POINTLIST);
+        System.out.println("Point");
+        Object[] pointVol = runTest(path, Image.VolumeTypes.POINTLIST,true);
         System.out.println(" ");
 
         // Testing Quadtree
-        System.out.println("QuadTreeVolume");
-        runTest(path,Image.VolumeTypes.QUADTREE);
+        System.out.println("Quadtree");
+        Object[] quadtreeVol = runTest(path,Image.VolumeTypes.QUADTREE,true);
         System.out.println(" ");
 
         // Testing Octree
-        System.out.println("OctreeVolulme");
-        runTest(path,Image.VolumeTypes.OCTREE);
+        System.out.println("Octree");
+        Object[] octreeVol = runTest(path,Image.VolumeTypes.OCTREE,true);
         System.out.println(" ");
+
+//        // Testing Optimised
+//        System.out.println("Optimised");
+//        Obj obj = runTest(path,Image.VolumeTypes.OPTIMISED,false);
+//        System.out.println("    Selected volume "+obj.getVolumeType());
+//        System.out.println(" ");
 
     }
 
-    public static void runTest(String path, String type) {
+    public static Object[] runTest(String path, String type, boolean verbosePerformance) {
         // Creating a new workspace
         Workspace workspace = new Workspace(0,null,1);
         ModuleCollection modules = new ModuleCollection();
@@ -57,10 +87,13 @@ public class PerformanceTest {
         workspace.addImage(image);
 
         // Creating the object
-        Obj obj = getObject(workspace,modules,type);
+        Obj obj = getObject(workspace,modules,type,verbosePerformance);
 
         // Testing object memory properties
-        evaluateObjectMemory(obj);
+        long memory = evaluateObjectMemory(obj,verbosePerformance);
+
+        // When running Optimised volume we don't need to re-run these tests
+        if (!verbosePerformance) return new Object[]{obj,memory};
 
         // Testing full coordinate access time (via iterator)
         evaluateFullAccessTime(obj);
@@ -68,9 +101,11 @@ public class PerformanceTest {
         // Testing random coordinate access time
         evaluateRandomAccessTime(obj);
 
+        return new Object[]{obj,memory};
+
     }
 
-    public static Obj getObject(Workspace workspace, ModuleCollection modules, String type) {
+    public static Obj getObject(Workspace workspace, ModuleCollection modules, String type, boolean verbosePerformance) {
         // Initialising IdentifyObjects
         IdentifyObjects identifyObjects = new IdentifyObjects(modules);
         identifyObjects.updateParameterValue(IdentifyObjects.INPUT_IMAGE,INPUT_IMAGE);
@@ -86,18 +121,21 @@ public class PerformanceTest {
         Obj obj = workspace.getObjectSet(OUTPUT_OBJECTS).getFirst();
 
         // Reporting creation time
-        System.out.println("    Load objects time = "+((endTime-startTime)/1E6)+" ms");
+        if (verbosePerformance) System.out.println("    Load objects time = "+((endTime-startTime)/1E6)+" ms");
 
         return obj;
 
     }
 
-    public static void evaluateObjectMemory(Obj obj) {
+    public static long evaluateObjectMemory(Obj obj, boolean verbosePerformance) {
         // Reporting Obj memory usage
-        System.out.println("    Memory usage = "+(((double)SizeEstimator.estimate(obj)/1048576d))+" MB");
+        long bytes = SizeEstimator.estimate(obj);
+        if (verbosePerformance) System.out.println("    Memory usage = "+(((double) bytes/1048576d))+" MB");
 
         // Reporting number of Obj elements (points, nodes, etc.)
-        System.out.println("    Number of elements = "+obj.getNumberOfElements());
+        if (verbosePerformance) System.out.println("    Number of elements = "+obj.getNumberOfElements());
+
+        return bytes;
 
     }
 
@@ -137,6 +175,38 @@ public class PerformanceTest {
         long endTime = System.nanoTime();
 
         System.out.println("    Full access time = "+((endTime-startTime)/1E6)+" ms");
+
+    }
+
+    public static void reportDistribution(Obj obj) {
+        CumStat csX = new CumStat();
+        CumStat csY = new CumStat();
+        CumStat csZ = new CumStat();
+
+        for (Point<Integer> point:obj.getCoordinateStore()) {
+            csX.addMeasure(point.getX());
+            csY.addMeasure(point.getY());
+            csZ.addMeasure(point.getZ());
+        }
+
+        double dppXY = obj.getDppXY();
+        double dppZ = obj.getDppZ();
+        int nPoints = (int) csX.getN();
+        double xStdev = csX.getStd();
+        double xMin = csX.getMin()-csX.getMean();
+        double xMax = csX.getMax()-csX.getMean();
+        double yStdev = csY.getStd();
+        double yMin = csY.getMin()-csY.getMean();
+        double yMax = csY.getMax()-csY.getMean();
+        double zStdev = csZ.getStd();
+        double zMin = csZ.getMin()-csZ.getMean();
+        double zMax = csZ.getMax()-csZ.getMean();
+
+
+        System.out.print(dppXY+","+dppZ+","+nPoints+","+
+                xStdev+","+xMin+","+xMax+","+
+                yStdev+","+yMin+","+yMax+","+
+                zStdev+","+zMin+","+zMax);
 
     }
 }
